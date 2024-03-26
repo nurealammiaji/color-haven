e367cd4bbf8fd8e030301ed5aab3113cc32e73ca
nurealammiaji <nurealam.miaji@gmail.com>
2024-03-26 13:26:02 +0600

    color category and button added

--- src/components/ColorBox/ColorBox.jsx
+++ src/components/ColorBox/ColorBox.jsx
@@ -1,10 +1,26 @@
 
 const ColorBox = () => {
     return (
-        <div>
-            <div className="mt-40">
-                <p>Pick Color</p>
-                <input className="w-[250px]" type="color" name="color" id="color" />
+        <div className="mt-36 flex">
+            <div className="flex flex-col border p-10 rounded-2xl">
+                <div className="rounded-xl border p-5">
+                    <p className="text-lg mb-3">Pick Color</p>
+                    <input className="w-[250px]" type="color" name="color" id="color" />
+                </div>
+                <div className="mt-5 rounded-xl border p-5">
+                    <p className="text-lg mb-3">Pick Category</p>
+                    <select className="w-[250px] p-2" name="category" id="category">
+                        <option value="blue">Blue</option>
+                        <option value="red">Red</option>
+                        <option value="orange">Orange</option>
+                        <option value="green">Green</option>
+                        <option value="black">Black</option>
+                        <option value="white">White</option>
+                    </select>
+                </div>
+                <div className="text-center mt-5">
+                    <button className="border font-medium text-lg rounded-lg py-2 px-5 bg-purple-400 hover:bg-purple-600 hover:text-white">Add Color</button>
+                </div>
             </div>
         </div>
     );
