8c178414eb373c7453faa14ee98a9101d9014733
nurealammiaji <nurealam.miaji@gmail.com>
2024-03-29 19:45:33 +0600

    style modified

--- src/components/ColorBox/ColorBox.jsx
+++ src/components/ColorBox/ColorBox.jsx
@@ -3,7 +3,7 @@ const ColorBox = ({ fn }) => {
 
     const colorHandler = fn;
 
-    const picker = (e) => {
+    const pickerHandler = (e) => {
         e.preventDefault();
         const color = e.target.color.value;
         const category = e.target.category.value;
@@ -12,8 +12,8 @@ const ColorBox = ({ fn }) => {
 
     return (
         <div className="relative">
-            <h4 className="absolute bg-white text-xl px-5 py-2 top-[-23px] left-32 center font-semibold">Color Box</h4>
-            <form onSubmit={picker} className="flex flex-col p-10 border rounded-2xl">
+            <h4 className="absolute bg-white text-xl px-4 left-10 py-2 top-[-23px] center font-semibold">Color Box</h4>
+            <form onSubmit={pickerHandler} className="flex flex-col p-10 border rounded-2xl">
                 <div className="p-5 border rounded-xl">
                     <p className="mb-3 text-lg">Pick Color</p>
                     <input className="w-[250px] h-[35px]" type="color" name="color" id="color" />
@@ -21,18 +21,19 @@ const ColorBox = ({ fn }) => {
                 <div className="p-5 mt-5 border rounded-xl">
                     <p className="mb-3 text-lg">Pick Category</p>
                     <select className="w-[250px] p-2" name="category" id="category">
-                        <option value="violet">Violet</option>
-                        <option value="blue">Blue</option>
-                        <option value="green">Green</option>
-                        <option value="white">White</option>
-                        <option value="yellow">Yellow</option>
-                        <option value="orange">Orange</option>
-                        <option value="black">Black</option>
-                        <option value="red">Red</option>
+                        <option value="Black">Black</option>
+                        <option value="White">White</option>
+                        <option value="Red">Red</option>
+                        <option value="Blue">Blue</option>
+                        <option value="Green">Green</option>
+                        <option value="Violet">Violet</option>
+                        <option value="Yellow">Yellow</option>
+                        <option value="Orange">Orange</option>
+                        <option value="Brown">Brown</option>
                     </select>
                 </div>
                 <div className="mt-8 text-center">
-                    <button type="submit" className="px-5 py-2 text-lg bg-purple-400 border rounded hover:bg-purple-600 hover:text-white">Add Color</button>
+                    <button type="submit" className="px-5 py-2 text-lg border rounded bg-slate-200 hover:bg-purple-600 hover:text-white">Add Color</button>
                 </div>
             </form>
         </div>
--- src/components/ShowCase/ShowCase.jsx
+++ src/components/ShowCase/ShowCase.jsx
@@ -1,7 +1,8 @@
 
 const ShowCase = ({ colorCategory, deleteHandler }) => {
     return (
-        <div className="w-full p-5 border rounded-2xl">
+        <div className="relative w-full p-5 border rounded-2xl">
+            <h3 className="absolute bg-white px-4 left-12 top-[-15px] text-xl font-semibold">Color Showcase</h3>
             <div className="p-5">
                 {
                     (colorCategory) &&
@@ -12,7 +13,7 @@ const ShowCase = ({ colorCategory, deleteHandler }) => {
                             <p>{item.category}</p>
                         </div>
                         <div>
-                            <button onClick={() => deleteHandler(item.id)} className="px-3 py-1 border rounded hover:bg-red-600 hover:text-white">Delete</button>
+                            <button onClick={() => deleteHandler(item.id)} className="px-3 py-1 border rounded hover:bg-red-600 bg-slate-200 hover:text-white">Delete</button>
                         </div>
                     </div>)
                 }
